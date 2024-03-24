7248d5955620ff6f9615188b080f79c905cadd32
lucas see <seelcs12@gmail.com>
2024-03-24 16:02:45 -0700

    updating top bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,6 @@ import ColorModeSwitch from "./ColorModeSwitch";
 export const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="100px"></Image>
       <h2> Lucas See, M.S.</h2>
       <ColorModeSwitch />
     </HStack>
